e1d7888ab64a999977c5ffd2e1c9211eb29ea408
Deathman5000 <33112987+Deathman5000@users.noreply.github.com>
2018-04-21 14:57:47 -0500

    The completed lab

--- Lab9A/app/src/main/java/com/example/jh949711/phonebook/PhoneBookActivity.java
+++ Lab9A/app/src/main/java/com/example/jh949711/phonebook/PhoneBookActivity.java
@@ -1,13 +1,24 @@
 package com.example.jh949711.phonebook;
+/*
+    This app sets up the contact list for the phone call application. The user is able to add and
+    delete contacts.
+    Authors: Kyra Belanger, James Hund
+    Version: 04/21/18
+    Parts worked on: Kyra: Both xml files and the DisplayActivity
+                     James: PhoneBookActivity.java, MyContentProvider.java, Product.java
+    Grade deserved: Kyra:  100%
+                    James: 100%
+ */
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class PhoneBookActivity extends AppCompatActivity {
 
@@ -30,20 +41,67 @@ public class PhoneBookActivity extends AppCompatActivity {
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                MakeCall makeCall = new MakeCall(firstName.getText().toString(),
-                        lastName.getText().toString(), phoneNumber.getText().toString());
-                ContentValues values = new ContentValues();
+                if (firstName.getText().toString() == "" || lastName.getText().toString() == "" || phoneNumber.getText().toString() == ""){
+                    Toast.makeText(PhoneBookActivity.this,"Please enter all parameters", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Product product = new Product(firstName.getText().toString(),
+                            lastName.getText().toString(), (phoneNumber.getText().toString()).substring(0, 3) + "-" +
+                            (phoneNumber.getText().toString()).substring(3, 6) + "-" + (phoneNumber.getText().toString()).substring(6, 10));
+                    ContentValues values = new ContentValues();
+                    values.put(MyContentProvider.COLUMN_FIRST_NAME, product.getFirstName());
+                    values.put(MyContentProvider.COLUMN_LAST_NAME, product.getLastName());
+                    values.put(MyContentProvider.COLUMN_PHONE_NUMBER, product.getPNumber());
+                    Uri uri = getContentResolver().insert(MyContentProvider.CONTENT_URI, values);
+                    Toast.makeText(PhoneBookActivity.this, "New product " + product.getFirstName() + " " +
+                            product.getLastName() + " " + product.getPNumber() + " added", Toast.LENGTH_SHORT).show();
 
+                }
+            }
+        });
 
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String selection = "firstName = \"" + firstName.getText().toString() + "\" AND lastName = \""
+                        + lastName.getText().toString() + "\"";
+                int result =
+                        getContentResolver().delete(MyContentProvider.CONTENT_URI,selection,null);
+                if(result > 0){
+                    Toast.makeText(PhoneBookActivity.this,firstName.getText().toString() + " " +
+                            lastName.getText().toString() +  " was deleted from contacts.", Toast.LENGTH_SHORT).show();
+                    firstName.setText("");
+                    lastName.setText("");
+                    phoneNumber.setText("");
+                    firstName.requestFocus();
+
+                }
+                else
+                    Toast.makeText(PhoneBookActivity.this,"No match found", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        clearFieldButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstName.setText("");
+                lastName.setText("");
+                phoneNumber.setText("");
+                firstName.requestFocus();
+            }
+        });
+        showAllButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firstName.setText("");
+                lastName.setText("");
+                phoneNumber.setText("");
+                firstName.requestFocus();
+                Intent intent = new Intent(PhoneBookActivity.this, DisplayActivity.class);
+                startActivity(intent);
             }
         });
     }
 
-    public void show(View view){
-        firstName.setText("");
-        lastName.setText("");
-        firstName.requestFocus();
-        Intent intent = new Intent(this, DisplayActivity.class);
-    }
 
 }
--- Lab9B/app/src/main/java/com/example/jh949711/makeacall/CallActivity.java
+++ Lab9B/app/src/main/java/com/example/jh949711/makeacall/CallActivity.java
@@ -1,13 +1,89 @@
 package com.example.jh949711.makeacall;
+/*
+    This app takes the info from the PhoneBook app and the user can make a call or look up the contact.
+    Authors: Kyra Belanger, James Hund
+    Version: 04/21/18
+    Parts worked on: We both equally worked on both files in this app.
+    Grade deserved: Kyra:  100%
+                    James: 100%
+ */
 
+import android.content.ActivityNotFoundException;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class CallActivity extends AppCompatActivity {
+
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_FIRST_NAME = "firstName";
+    private static final String COLUMN_LAST_NAME = "lastName";
+    private static final String COLUMN_PHONE_NUMBER = "number";
+    EditText fName, lName;
+    Button find, call;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call);
+        fName = findViewById(R.id.firstName);
+        lName = findViewById(R.id.lastName);
+        find = findViewById(R.id.find);
+        call = findViewById(R.id.makeCall);
+        find.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String AUTHORITY = "com.example.jh949711.phonebook.MyContentProvider";
+                String TABLE_PRODUCT = "product";
+                Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" +TABLE_PRODUCT);
+                ContentResolver contentResolver = getContentResolver();
+                String [] projection = {COLUMN_ID, COLUMN_FIRST_NAME, COLUMN_LAST_NAME, COLUMN_PHONE_NUMBER};
+                Cursor cursor = contentResolver.query(CONTENT_URI,projection,null,null,null);
+                String s = "";
+                TextView tv = findViewById(R.id.phoneNumber);
+                if (!cursor.moveToFirst()) s = "no result to display";
+                else{
+                    do {
+                        s += String.format("%-10s\n",cursor.getString(3));
+                    } while (cursor.moveToNext());
+                }
+                tv.setText(s);
+            }
+        });
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String AUTHORITY = "com.example.jh949711.phonebook.MyContentProvider";
+                String TABLE_PRODUCT = "product";
+                Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" +TABLE_PRODUCT);
+                ContentResolver contentResolver = getContentResolver();
+                String [] projection = {COLUMN_ID, COLUMN_FIRST_NAME, COLUMN_LAST_NAME, COLUMN_PHONE_NUMBER};
+                Cursor cursor = contentResolver.query(CONTENT_URI,projection,null,null,null);
+                String s = "";
+                if (!cursor.moveToFirst()) s = "Name not in contacts";
+
+                else{
+                    String phone_number = cursor.getString(3);
+                    try {
+                        Uri uri = Uri.parse("tel:" + phone_number);
+                        Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                        Log.i("Phone Call App", phone_number);
+                        startActivity(intent);
+                    } catch (ActivityNotFoundException e) {
+                        Toast.makeText(getApplicationContext(), "Application failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
     }
 }
